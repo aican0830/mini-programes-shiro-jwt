74f400bcdaf02261144de4087fa2846cdda9e7e0
aidong <aidong@gmail.com>
2020-03-13 17:20:31 +0800

    Update

---
+++ src/main/java/com/demo/miniprogramesshirojwt/domain/repository/WxAccountRepository.java
@@ -0,0 +1,15 @@
+package com.demo.miniprogramesshirojwt.domain.repository;
+
+import com.demo.miniprogramesshirojwt.domain.entity.WxAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by EalenXie on 2018/11/26 10:32.
+ */
+public interface WxAccountRepository extends JpaRepository<WxAccount, Integer> {
+
+    /**
+     * 根据OpenId查询用户信息
+     */
+    WxAccount findByWxOpenid(String wxOpenId);
+}
